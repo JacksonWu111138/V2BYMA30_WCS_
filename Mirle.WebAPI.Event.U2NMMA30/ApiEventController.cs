96e17f526b7af1d342e8c3c041fdbebc46b4b104
AQ5 111138 吳沛霆(Jackson) <jackson@mirle.com.tw>
2022-08-02 12:03:04 +0800

    feat: 更新BufferStatusQuery；CVReceiveNewBinCmd添加ioType欄位

--- Mirle.WebAPI.Event.U2NMMA30/ApiEventController.cs
+++ Mirle.WebAPI.Event.U2NMMA30/ApiEventController.cs
@@ -3,7 +3,7 @@ using System.Data;
 using Mirle.DB.Object;
 using Mirle.Def;
 using Mirle.Def.U2NMMA30;
-using Mirle.WebAPI.Event.U2NMMA30.Models;
+using Mirle.WebAPI.Event.V2BYMA30.Models;
 using System.Web.Http;
 using Mirle.Structure;
 using Newtonsoft.Json;
--- Mirle.WebAPI.Event.U2NMMA30/Models/ShelfTransferInfo.cs
+++ Mirle.WebAPI.Event.U2NMMA30/Models/ShelfTransferInfo.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Mirle.WebAPI.Event.U2NMMA30.Models
+namespace Mirle.WebAPI.Event.V2BYMA30.Models
 {
     public class ShelfTransferInfo : BaseInfo
     {

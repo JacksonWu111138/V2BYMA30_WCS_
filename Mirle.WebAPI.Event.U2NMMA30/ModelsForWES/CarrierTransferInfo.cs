458e0df1ce25badb69217afbe670ff15eabf77b3
AQ5 111138 吳沛霆(Jackson) <jackson@mirle.com.tw>
2022-08-05 11:37:37 +0800

    feat: 新增接收WES API基本架構

---
+++ Mirle.WebAPI.Event.U2NMMA30/ModelsForWES/CarrierTransferInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mirle.WebAPI.Event.U2NMMA30.Models
+{
+    public class CarrierTransferInfo : BaseInfo
+    {
+        public string carrierId { get; set; }
+        public string fromLocation { get; set; } 
+        public string toLocation { get; set; }
+        public string priority { get; set; }
+    }
+}

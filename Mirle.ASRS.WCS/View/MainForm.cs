a4d0741df1b46cde491c0fbcd0f8809371be4cec
AQ5 104018 蔡君彥 <iantsai@mirle.com.tw>
2022-08-11 17:17:32 +0800

    feat: 啟動API的被動接收port

--- Mirle.ASRS.WCS/View/MainForm.cs
+++ Mirle.ASRS.WCS/View/MainForm.cs
@@ -520,9 +520,9 @@ namespace Mirle.ASRS.WCS.View
             FunAsrsCmdInit();
             middle = new MidHost(ConveyorDef.GetAllNode(), clInitSys.AgvApi_Config, PCBA, Box, 
                 ConveyorDef.DeviceID_AGV, ConveyorDef.DeviceID_Tower, clInitSys.DbConfig, clInitSys.AgvApi_Config, clInitSys.TowerApi_Config);
-            //_unityContainer = new UnityContainer();
-            //_unityContainer.RegisterInstance(new WCSController());
-            //_webApiHost = new WebApiHost(new Startup(_unityContainer), clInitSys.WcsApi_Config.IP);
+            _unityContainer = new UnityContainer();
+            _unityContainer.RegisterInstance(new WCSController());
+            _webApiHost = new WebApiHost(new Startup(_unityContainer), clInitSys.WcsApi_Config.IP);
             //clearCmd = new DB.ClearCmd.Proc.clsHost();
         }
 
